85f4eb887289baf5e1f09edad40cdfbc33bb5f13
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-30 11:42:00 +0530

    Answer section with the perfect merge image is set

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import './App.css';
+import puzzle from "./puzzle.jpg";
 
-function App() {
-  return (
-    <div className="App">
-    <div class="container">
-      <div class="row">
-        <div className="col-md-9">
-          <div className="row">
-            <div className="col-md-12 d-flex  justify-content-center align-items-center" style={{backgroundColor :"yellow"}}>
-              &nbsp;              
+class App extends React.Component {
+  render() {
+    let aindex = 0;
+    return (
+      <div className="App">
+        <div class="container">
+          <div class="row">
+            <div className="col-md-9">
+              <div className="row">
+                <div className="col-md-12 d-flex  justify-content-center align-items-center">
+                  &nbsp;
             </div>
-          </div>
-        </div>
-        <div className="col-md-3 text-center">
-          <div className="row d-flex justify-content-center align-items-top text-white" style={{backgroundColor :"green"}}>
-            <div className="col-md-12">
-              &nbsp;            
+              </div>
+            </div>
+            <div className="col-md-3 text-center">
+              <div className="row d-flex justify-content-center align-items-center text-white">
+                <div className="col-md-12">
+                  <table className="anstable" style={{ width: "100%", border: "2px solid white", backgroundImage: "url(" + puzzle + ")", backgroundSize: "100% 100%", backgroundRepeat: "no-repeat" }}>
+                    <tbody>
+                      {[...Array(5)].map((data, index) => {
+                        return <tr>
+                          {[...Array(8)].map((sdata, sindex) => {
+                            aindex = aindex + 1;
+                            if (aindex === 40) {
+                              return <td id={"box_" + aindex} style={{ minWidth: "10px", minHeight: "10px", backgroundColor: "#FFF" }}>
+                                &nbsp;
+                              </td>
+                            } else {
+                              return <td id={"box_" + aindex} style={{ minWidth: "10px", minHeight: "10px" }}>
+                                &nbsp;
+                              </td>
+                            }
+                          })}
+                        </tr>
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  );
+    );
+  }
 }
 
 export default App;
