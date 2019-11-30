73ce3db390794f8514221bddd17fbf1cf91c1b4f
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-30 11:49:41 +0530

    shuffle function integration in which card will be suffeled and random blocks can be appear

--- src/App.js
+++ src/App.js
@@ -17,10 +17,31 @@ class App extends React.Component {
       return index + 1;
     });
 
+    const shuffledCards =  await this.shuffle(arr);
+  
     this.setState({
-      cardPositions: arr,
+      cardPositions: shuffledCards,
       isInitialize : true
     });
+  }
+
+  shuffle = async (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
   }
 
   render() {
